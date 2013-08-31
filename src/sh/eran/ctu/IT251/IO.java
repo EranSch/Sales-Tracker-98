55c63dcd016c29c9ddef3833fae78ccc644599f8
Eran <me@eran.sh>
2013-08-31 15:05:34 -0700

    Imported IO class, for handling user input and such.

---
+++ src/sh/eran/ctu/IT251/IO.java
@@ -0,0 +1,130 @@
+package sh.eran.ctu.IT251;
+
+import java.util.Scanner;
+
+/**
+ * 
+ * @author eran
+ * @version 1.1
+ * 
+ * This class was developed to remove some of the mundane tasks involved in
+ * communicating with the user. Expect to see simplifications in terms of
+ * writing to the console as well as means of getting responses from the user
+ * The goal here is to continually build on this class and wind up with 
+ * a solid tool by the end of my degree. 
+ */
+
+public class IO {
+    
+    /*
+     * ============================= IO.print ==================================
+     */ 
+    
+    /*
+     * I got sick of typing "System.out.println()" all the time. I think this was 
+     * the main reason I started this class. Ahhh, I love this stuff!
+     * 
+     * Give it a string and watch is print on the console-- Magic!
+     * 
+     */
+    
+    public static void print( String message ){
+        System.out.println( message );
+    }
+    
+    
+    /*
+     * =========================== IO.clearConsole =============================
+     */ 
+    
+    /*
+     * Another mundane task, So strange there isn't something in API for this...
+     * All this does is clears the console for pretty navigation. Also magic.
+     * 
+     */    
+    
+    public static void clearConsole(){
+        
+        for( int i=0; i < 1000; i++ ){
+            IO.print("\n");
+        }
+        
+    }
+
+    
+    /*
+     * =============================== IO.getInt ===============================
+     */ 
+    
+    /*
+     * This is a little more interesting, the method takes two arguments-- A 
+     * string and an integer. While this could be used for other tasks, the 
+     * original implemnetaion was for handling user response in terms of an 
+     * indexed menu. The idea being; print the menu, and wait for the user to
+     * pick an option. The first arguement, String, is the message you'd like to
+     * pass to user (e.g. "Please select an item from the item" ). The following 
+     * argument sets the ceiling of the range of numbers available for selection.
+     * 
+     * @returns int
+     * 
+     */    
+    
+    public static int getInt( String message, int ceiling ) {
+        
+        Scanner sc = new Scanner(System.in);
+        int userInteger;
+        
+        do // This loop will enforce the input falls within the defined range...
+        {
+            
+            // Print the messages
+            IO.print(message);
+            IO.print( "Enter a number (1 - " + ceiling + ")");
+            
+            // This inner loop will also encorce the input be an Integer.
+            while( !sc.hasNextInt() )
+            {
+                IO.print( "Enter a NUMBER between 1 and " + ceiling + "." );
+                sc.next();
+            }
+            
+            userInteger = sc.nextInt();
+        
+        } while ( userInteger < 1 || userInteger > ceiling );
+        
+        return userInteger;
+        
+    }
+    
+
+    /*
+     * ============================= IO.getConfirm =============================
+     */ 
+    
+    /*
+     * If you thought that was fun, check this method out! This also takes a 
+     * String message as the input however it returns a boolean. This assumes
+     * you're posing a y/n type of question to the user.
+     * 
+     * @returns boolean
+     * 
+     */    
+
+    public static boolean getConfirm( String message ) {
+        
+        String userReply; // Going to store the response here for evaluation
+        Scanner sc = new Scanner(System.in);
+        
+        do // Loop to enforce proper y/n response from user. 
+        {
+            IO.print( message );
+            IO.print( "Please enter 'y' or 'n'" ); 
+            userReply = sc.nextLine().toLowerCase(); 
+        }
+        while( !userReply.equals("y") && !userReply.equals("n") );
+        
+        return userReply.equals("y");
+        
+    } // end method
+    
+}
